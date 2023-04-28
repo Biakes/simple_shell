bdb56d48edbdbe4576d6c585154e13bb10527fcf
Biakes <akinshiloabimbola@gmail.com>
2023-04-27 20:09:24 -0700

    task 0

---
+++ README.md
@@ -0,0 +1,1 @@
+Creating a custom made shell environment.
---
+++ betty_simple_code.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 is success
+ */
+int main(void)
+{
+	printf("betty\n");
+	return (0);
+}
